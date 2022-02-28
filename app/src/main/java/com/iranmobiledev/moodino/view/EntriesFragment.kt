195f9950c1f44df6cd6d73497627a0eaf6d22871
MohammadJavad <mohammadjavadkhoshneshin@gmail.com>
2022-02-28 07:33:58 -0800

    'add_entries_fragment_and_empty_state_fragment'

---
+++ app/src/main/java/com/iranmobiledev/moodino/view/EntriesFragment.kt
@@ -0,0 +1,18 @@
+package com.iranmobiledev.moodino.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.iranmobiledev.moodino.base.BaseFragment
+import com.iranmobiledev.moodino.base.BaseView
+
+class EntriesFragment : BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
