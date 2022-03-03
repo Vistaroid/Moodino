e9092b8a04141b0a1267afdc1b43e0d039b43b0a
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2022-03-03 12:54:54 +0330

    added bottom nav bar

--- app/src/main/java/com/iranmobiledev/moodino/MainActivity.kt
+++ app/src/main/java/com/iranmobiledev/moodino/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.iranmobiledev.moodino
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val fab : FloatingActionButton = findViewById(R.id.fab)
+
+        navView.background = null
+        fab.background = resources.getDrawable(R.drawable.custom_add_button_background)
+
+
     }
 }
