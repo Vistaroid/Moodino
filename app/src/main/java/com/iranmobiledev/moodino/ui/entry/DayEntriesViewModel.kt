42741fe261c4db5e9fc05786d24017d056d31c0f
mahdi sayeri <mahdisayeri@gmail.com>
2022-05-14 16:56:23 +0430

    fix Bug

--- app/src/main/java/com/iranmobiledev/moodino/ui/entry/DayEntriesViewModel.kt
+++ app/src/main/java/com/iranmobiledev/moodino/ui/entry/DayEntriesViewModel.kt
@@ -1,8 +1,7 @@
 package com.iranmobiledev.moodino.ui.entry
 
 import com.iranmobiledev.moodino.base.BaseViewModel
-import com.iranmobiledev.moodino.repository.entry.EntryRepository
 
-class DayEntriesViewModel(private val entryRepo: EntryRepository) : BaseViewModel() {
+class DayEntriesViewModel : BaseViewModel() {
 
 }
