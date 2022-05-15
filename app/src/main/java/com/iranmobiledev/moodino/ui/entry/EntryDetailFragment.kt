bd6e9f565f6a574bf23a3c73d96a35b2a943e757
mrkhoshneshin <mrkhoshneshin1@gmail.com>
2022-05-15 01:54:48 -0700

    'updateIcons'

--- app/src/main/java/com/iranmobiledev/moodino/ui/entry/EntryDetailFragment.kt
+++ app/src/main/java/com/iranmobiledev/moodino/ui/entry/EntryDetailFragment.kt
@@ -1,6 +1,9 @@
 package com.iranmobiledev.moodino.ui.entry
 
+import android.content.SharedPreferences
+import android.graphics.drawable.ClipDrawable.HORIZONTAL
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +15,7 @@ import com.iranmobiledev.moodino.R
 import com.iranmobiledev.moodino.base.BaseFragment
 import com.iranmobiledev.moodino.data.Entry
 import com.iranmobiledev.moodino.databinding.EntryDetailFragmentBinding
-import com.iranmobiledev.moodino.utlis.ENTRY
-import com.iranmobiledev.moodino.utlis.EmojiFactory
-import com.iranmobiledev.moodino.utlis.EmojiInterface
-import com.iranmobiledev.moodino.utlis.ImageLoadingService
+import com.iranmobiledev.moodino.utlis.*
 import com.vansuita.pickimage.bundle.PickSetup
 import com.vansuita.pickimage.dialog.PickImageDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -29,6 +29,7 @@ class EntryDetailFragment : BaseFragment(),
     private val entryDetailViewModel: EntryDetailViewModel by viewModel()
     private val imageLoader: ImageLoadingService by inject()
     private var entry = Entry()
+    private val sharedPref: SharedPreferences by inject()
 
     override fun onStart() {
         super.onStart()
@@ -48,7 +49,7 @@ class EntryDetailFragment : BaseFragment(),
     }
 
     private fun setupUi(emojiFactory: EmojiInterface) {
-        val icon = when(entry.emojiValue){
+        val icon = when (entry.emojiValue) {
             1 -> emojiFactory.getEmoji(entry.emojiValue.toFloat())
             2 -> emojiFactory.getEmoji(entry.emojiValue.toFloat())
             3 -> emojiFactory.getEmoji(entry.emojiValue.toFloat())
@@ -57,6 +58,9 @@ class EntryDetailFragment : BaseFragment(),
             else -> null
         }
         icon?.let { binding.entryiconDetail.setImageResource(it.image) }
+        val language = sharedPref.getInt(LANGUAGE, PERSIAN)
+        if (language == PERSIAN)
+            binding.backIv.rotation = 180f
     }
 
     private fun setupUtil() {
@@ -73,7 +77,9 @@ class EntryDetailFragment : BaseFragment(),
     }
 
     private fun navigateToEntryFragment() {
-        findNavController().navigate(R.id.action_entryDetailFragment_to_entriesFragment, Bundle().apply {
+        findNavController().navigate(
+            R.id.action_entryDetailFragment_to_entriesFragment,
+            Bundle().apply {
                 putParcelable(ENTRY, entry)
             })
     }
--- app/src/main/java/com/iranmobiledev/moodino/utlis/key.kt
+++ app/src/main/java/com/iranmobiledev/moodino/utlis/key.kt
@@ -2,3 +2,5 @@ package com.iranmobiledev.moodino.utlis
 
 const val LANGUAGE = "language"
 const val ENTRY = "entry"
+const val PERSIAN = 1
+const val ENGLISH = 2
