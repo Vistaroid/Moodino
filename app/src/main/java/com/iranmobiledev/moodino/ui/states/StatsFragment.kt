76633bdcea02bb5acb8f78654a158ce14ae9362b
Tayeb-shahbakhsh <shahbakshtayyeb@gmail.com>
2022-06-04 14:08:10 -0600

    color bug fixed in stats fragment

--- app/src/main/java/com/iranmobiledev/moodino/ui/states/StatsFragment.kt
+++ app/src/main/java/com/iranmobiledev/moodino/ui/states/StatsFragment.kt
@@ -219,12 +219,19 @@ class StatsFragment : BaseFragment(), ChangeCurrentMonth {
             binding.fifthDayFrameLayout
         )
 
-        val nightMode = when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
-            Configuration.UI_MODE_NIGHT_YES -> true
-            Configuration.UI_MODE_NIGHT_NO -> false
-            else -> false
-        }
+        val nightMode =
+            when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                Configuration.UI_MODE_NIGHT_YES -> true
+                Configuration.UI_MODE_NIGHT_NO -> false
+                else -> false
+            }
 
+        views.forEach {
+            it.background =
+                if (nightMode) resources.getDrawable(R.drawable.circle_shape_dark) else resources.getDrawable(
+                    R.drawable.circle_shape
+                )
+        }
     }
 
 
